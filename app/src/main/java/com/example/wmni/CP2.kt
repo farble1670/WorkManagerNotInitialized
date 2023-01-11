1e0f355146c533fe6ac4a6ac68caa9771371fc0b
Jeff Blattman <jeff@clover.com>
2023-01-10 16:51:56 -0800

    Initial commit

---
+++ app/src/main/java/com/example/wmni/CP2.kt
@@ -0,0 +1,45 @@
+package com.example.wmni
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import androidx.work.WorkManager
+
+class CP2: ContentProvider() {
+  override fun onCreate(): Boolean {
+    WorkManager.getInstance(context!!)
+    return true
+  }
+
+  override fun query(
+    uri: Uri,
+    projection: Array<out String>?,
+    selection: String?,
+    selectionArgs: Array<out String>?,
+    sortOrder: String?
+  ): Cursor? {
+    TODO("Not yet implemented")
+  }
+
+  override fun getType(uri: Uri): String? {
+    TODO("Not yet implemented")
+  }
+
+  override fun insert(uri: Uri, values: ContentValues?): Uri? {
+    TODO("Not yet implemented")
+  }
+
+  override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+    TODO("Not yet implemented")
+  }
+
+  override fun update(
+    uri: Uri,
+    values: ContentValues?,
+    selection: String?,
+    selectionArgs: Array<out String>?
+  ): Int {
+    TODO("Not yet implemented")
+  }
+}
